d8bbd6e805dcd62a03d984879fedb1b114e66256
ShaunW <shaunw@ShaunWdeMacBook-Pro.local>
2022-02-26 21:29:30 -0500

    updateSocketEvent

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { createServer } from 'http';
 const app = express();
 const httpServer = createServer(app);
 
+const members = [];
 // app.get('/', (req, res) => {
 //         console.log('you connected');
 //         res.send('you connected');
@@ -42,18 +43,62 @@ httpServer.listen(port, () => {
 // - a message, a typing event, a connection/ disconnection etc. io manages
 //all og those events and can broadcast them to everyone connected to the server.
 
+const LOGIN = 'login';
+const LOGIN_SUCCESS = 'loginsuccess';
+const LOGIN_FAILED = 'loginfailed'
+const MEMBERS = 'members';
+const MESSAGES = 'messages';
+const JOIN = 'join';
+const LEAVE = 'leave';
+const DISCONNECT = 'disconnect';
+const TYPING = 'typing';
+const END_TYPING = 'endtyping';
+
 io.on('connection', (socket) => {
     console.log('a user conneted!');
 
-    socket.emit("CONNECTED", socket.id);
+    socket.on(LOGIN, (data) => {
 
-    socket.on('SEND_MESSAGE', function(data) {
-        console.log('SEND_MESSAGE event!', data);
+        //check if the username exist or not, if already exist, tell the user.
+        if (members.findIndex((m) => m.username === data.username) > -1) {
+            return socket.emit(LOGIN_FAILED, { msg: "Username already exist!! Try different One" })
+        }
 
-        io.emit('MESSAGE', data);
+        let user = {...data, id: socket.id }
+        socket.emit(LOGIN_SUCCESS, user)
+
+        //when user log in successfully, notice welcome words.
+        members.push(user);
+        io.emit(JOIN, { msg: 'Welcome ' + data.username + ' joined this room!!' });
+        //tell all users someone join in the room, broadcasting events :io.emit
+        io.emit(MEMBERS, members);
+
+        socket.username = data.username
     })
 
-    socket.on('USER_TYPING', (data => {
-        io.emit('SOMEONE_TYPING', data);
+    // disconnect function
+    socket.on(DISCONNECT, () => {
+        let idx = members.findIndex(e => e.username === socket.username)
+        if (idx === -1) {
+            return;
+        }
+        members.splice(idx, 1)
+        io.emit(LEAVE, { msg: socket.username + ' left the room!!' })
+        io.emit(MEMBERS, members)
+    })
+
+    socket.on(MESSAGES, function(data) {
+        console.log('SEND_MESSAGE event!', data);
+
+        io.emit(MESSAGES, data);
+    })
+
+    //mention users when someone is typing
+    socket.on(TYPING, (data => {
+        io.emit(TYPING, { id: socket.id });
+    }))
+
+    socket.on(END_TYPING, (data => {
+        io.emit(END_TYPING, { id: socket.id });
     }))
 });
